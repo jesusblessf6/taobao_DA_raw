1e8ee6d436246bd75698dd570de928a43c3cc909
yirun0701 <yirun0701@icloud.com>
2014-02-17 17:58:06 +0800

    update

--- crawlers/basic/skuCrawler.js
+++ crawlers/basic/skuCrawler.js
@@ -5,4 +5,57 @@ exports.start = function(skuMeta, outercallback){
 	var async = require('async');
 	var SkuMeta = require('../../models/skuMeta');
 	var Sku = require('../../models/sku');
+	var pageCount = 1;
+
+	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+
+	async.series([
+
+		//open the sku meta page
+		function(callback){
+			driver.get(settings.itemMetaDetailPageUrl + "&spuid=" + skuMeta.itemMetaTid + "&spec=" + skuMeta.tid).then(callback);
+		},
+
+		//get the page info
+		function(callback){
+			driver.isElementPresent({className : "vm-page-text"}).then(function(existed){
+				if(existed){
+					//normal page.
+					driver.findElements({className : "vm-page-text"}).then(function(elements){
+						async.each(elements, function(element, callback){
+							element.getTetx().then(function(text){
+
+							});
+						}, function(err){
+							if(err){
+								console.log(err);
+							}
+							callback();
+						});
+					});
+				}else{
+					//abnormal page
+					driver.close();
+					driver.quit();
+					driver = null;
+					outercallback();
+				}
+			}).then(callback);
+		},
+
+		function(callback){
+			driver.close();
+			driver.quit();
+			driver = null;
+			callback();
+		}
+
+	], function(err){
+		if(err){
+			console.log(err);
+		}
+
+		outercallback();
+	});
+
 };
